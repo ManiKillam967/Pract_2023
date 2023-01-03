631d7fcd768aeaa6e9f5c921e91c1c462fec006e
manikanta <manikillam967>
2023-01-03 22:22:09 +0530

    newCollection

---
+++ naukri/src/test/java/DEmo_collections/Arraylis.java
@@ -0,0 +1,16 @@
+package DEmo_collections;
+
+import java.util.ArrayList;
+
+public class Arraylis {
+public static void main(String[] args) {
+	
+	
+	ArrayList<String> g1 = new ArrayList<String>();
+    
+	g1.add("aaaaa");
+	g1.add("bbbbb");
+	System.out.println(g1);
+	
+}
+}
