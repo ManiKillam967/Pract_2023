95062344e8ae8fa2129135a4fe8d641da6d6286d
manikanta <manikillam967>
2023-01-03 22:15:08 +0530

    FirstCOmmit

---
+++ naukri/src/main/java/mani_sel/naukri/App.java
@@ -0,0 +1,13 @@
+package mani_sel.naukri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
