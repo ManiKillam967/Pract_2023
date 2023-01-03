a5e694f3c847ed2b60d471442abd30057763f3ad
ManiKillam967 <114490299+ManiKillam967@users.noreply.github.com>
2023-01-03 23:31:52 +0530

    Update Arraylis.java

--- naukri/src/test/java/DEmo_collections/Arraylis.java
+++ naukri/src/test/java/DEmo_collections/Arraylis.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 	
 	
 	ArrayList<String> g1 = new ArrayList<String>();
-    
+        //this is manikanta
 	g1.add("aaaaa");
 	g1.add("bbbbb");
 	System.out.println(g1);
